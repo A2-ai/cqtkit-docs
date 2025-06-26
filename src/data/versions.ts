62f9dfa8e14904410ee58256399d1e8e8d5dc31c
Matt Smith <msmith21794@gmail.com>
2025-06-26 18:02:05 -0400

    versions

--- src/data/versions.ts
+++ src/data/versions.ts
@@ -1,5 +1,4 @@
 export const VERSIONS = [
-	{ tag: '0.0.3', label: 'v0.1.3' },
-	{ tag: '0.0.2', label: 'v0.1.2' },
-	{ tag: '0.0.1', label: 'v0.1.1' },
+	{ tag: 'v0.0.2', label: 'v0.1.2' },
+	{ tag: 'v0.0.1', label: 'v0.1.1' },
 ]
